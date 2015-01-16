7818b4b13142f2c4146c2b31732d8375c02af44e
luowei <745054025@qq.com>
2015-01-16 11:51:35 +0800

    Initial Commit

---
+++ App-Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App-Base
+//
+//  Created by luowei on 15/1/16.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App-Base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  App-Base
+//
+//  Created by luowei on 15/1/16.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
